0bceaf8e8c8a4453032a0f197dcf3e04263a2789
Olivers De Abreu <oliversd@gmail.com>
2020-03-27 16:30:44 -0300

    Add sum function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log("Hola desde index");
 
-const add = (x, y) => {};
+const add = (x, y) => {
+  return x + y;
+};
 const substract = (x, y) => {};
 const multiply = (x, y) => {};
 const divide = (x, y) => {};
